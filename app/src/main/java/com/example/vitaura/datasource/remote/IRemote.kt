7ff859028d3ea9ad246a6b2e87d92d90db34adc3
Ruslan Ivanykin <pandatek@inbox.ru>
2020-11-22 01:55:51 +0800

    Инициализация проекта

---
+++ app/src/main/java/com/example/vitaura/datasource/remote/IRemote.kt
@@ -0,0 +1,4 @@
+package com.example.vitaura.datasource.remote
+
+interface IRemote {
+}
---
+++ app/src/main/java/com/example/vitaura/datasource/remote/RemoteDataSource.kt
@@ -0,0 +1,5 @@
+package com.example.vitaura.datasource.remote
+
+class RemoteDataSource: IRemote {
+    //Связь с сервером
+}
