cf740a703f100f9770a77bda18ad752301f57d69
Ruslan Ivanykin <pandatek@inbox.ru>
2020-12-03 17:59:26 +0800

    Начал верстать главную страницу

--- app/src/main/java/com/example/vitaura/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/vitaura/ui/base/BaseActivity.kt
@@ -3,6 +3,8 @@ package com.example.vitaura.ui.base
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import com.example.vitaura.R
 import com.example.vitaura.databinding.ActivityMainBinding
 import com.example.vitaura.ui.main.MainFragment
@@ -10,17 +12,29 @@ import com.google.android.material.navigation.NavigationView
 
 class BaseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    private val viewBind by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewBind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBind.root)
+        setSupportActionBar(viewBind.toolbar)
+        supportActionBar?.title = ""
+        val drawerToggle = ActionBarDrawerToggle(this, viewBind.drawerLayout, viewBind.toolbar, R.string.message_1, R.string.message_1)
+        viewBind.drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        viewBind.navigator.setNavigationItemSelectedListener(this)
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_container, MainFragment())
             .commit()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-
+        viewBind.drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+
     }
 }
