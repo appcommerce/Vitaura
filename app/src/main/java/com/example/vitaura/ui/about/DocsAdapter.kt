166c10af3de0a63d427485f2703bb5bc3d07a3ae
Ruslan Ivanykin <pandatek@inbox.ru>
2020-12-29 05:02:28 +0800

    Полностью подключен раздел с лицензией

--- app/src/main/java/com/example/vitaura/ui/about/DocsAdapter.kt
+++ app/src/main/java/com/example/vitaura/ui/about/DocsAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.vitaura.ui.about
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vitaura.databinding.ItemDocsBinding
 import com.example.vitaura.pojo.Doc
+import com.squareup.picasso.Picasso
 
 class DocsAdapter: RecyclerView.Adapter<DocsAdapter.DocsViewHolder>() {
     private var docs = listOf<Doc>()
@@ -15,7 +17,14 @@ class DocsAdapter: RecyclerView.Adapter<DocsAdapter.DocsViewHolder>() {
     inner class DocsViewHolder(itemViewBind: ItemDocsBinding): RecyclerView.ViewHolder(itemViewBind.root){
         private val layout = itemViewBind
         fun bind(doc: Doc) = with(itemView){
-
+            Picasso.get()
+                .load(doc.url)
+                .into(layout.docImg)
+            layout.docTitle.text = doc.title
+            layout.openDoc.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) layout.docImg.visibility = View.VISIBLE
+                else layout.docImg.visibility = View.GONE
+            }
         }
     }
 
